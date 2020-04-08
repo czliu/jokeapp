793503c3d7bb00661a6a0b883ce75809b25e805b
Alex Liu <liucongzhi@gmail.com>
2020-04-07 22:46:38 -0400

    create the spring boot project

---
+++ src/test/java/io/github/czliu/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.czliu.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
